4f5335ae25c5fdd76a3fd2cc59f348ba012a8884
lgloredana <elia_earth@yahoo.com>
2018-12-15 12:54:25 +0000

    split app.js into components and add functionality for the chat

---
+++ src/NewMessage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+class NewMessage extends Component {
+    /*
+  If the user did not type anything, he/she should not be
+  allowed to submit.
+  */
+  state = { 
+    newMessage: "",
+  }
+  handleChange(event){
+    this.setState({
+    	newMessage: event.target.value
+  	});
+  }
+
+  handleNewMsg = (event) => {
+    event.preventDefault();
+    this.props.handleNewMsg(this.state.newMessage);
+  }
+
+  render() {
+	const {newMessage} = this.state;
+    return (
+      <form className="input-group" onSubmit={this.handleNewMsg}>
+          <input type="text" className="form-control" placeholder="Enter your message..." 
+    		onChange={(event) => this.handleChange(event)} />
+          <div className="input-group-append">
+          	<button className="btn submit-button" disabled={newMessage === ''}>
+    				SEND
+    		</button>
+    	</div>
+      </form>
+    )
+  }
+}
+
+export default NewMessage
